9db90e695b3432df7fbafc2b093dd9b021165c44
willwos <103256644+willwos@users.noreply.github.com>
2022-11-27 11:12:31 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-var randomNumber = function(start, range) {
+// const rollButton = document.querySelector('#rollButton');
+
+// rollButton.addEventListener('click', () => {
+//     randomNumber(1, 100);
+// });
+
+const resultNumber = document.querySelector('#resultNumber');
+
+const randomNumber = function(start, range) {
     let getRandom = Math.floor((Math.random() * range) + start);
     while (getRandom > range) {
         getRandom = Math.floor((Math.random() * range) + start);
     }    
-    return getRandom;
+    console.log('working', getRandom);
+    //return getRandom;
+    resultNumber.innerText = getRandom;
 }
-console.log(randomNumber(1, 100));
+// console.log(randomNumber(1, 100));
